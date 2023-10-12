572cf72c5067c3d6e11e431587ea04c8bd06878a
RodCato <code@eliment.org>
2023-10-11 22:01:46 -0500

    Added 1st 3 components as pages to a nextjs project

---
+++ pages/page3.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Page3 = () => {
+  return (
+    <div>
+      <h1>This is the Test Page 3</h1>
+      <p>Welcome to the test page 3 and your 3rd component built on next.js</p>
+    </div>
+  );
+};
+
+export default Page3;
+
---
+++ pages/test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            <h1>This is the Test Page</h1>
+            <p>Welcome to the test page of your Next.js project!</p>
+        </div>
+    );
+};
+
+export default Test;
+ 
