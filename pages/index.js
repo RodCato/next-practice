0b3f193d710fde280dd915d87256dd233f44528e
RodCato <code@eliment.org>
2023-10-12 21:51:00 -0500

    testing gatsby code with nextjs project

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,81 @@
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import Experience from "./experience";
+import * as React from "react";
+// import { Helmet } from "react-helmet-async";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
+// import Header from "./header";
+// import "./layout.css";
+// import About from "./about";
 import Projects from "./projects";
-import Education from "./education";
 import Skills from "./skills";
+// import Connect from "./connect";
+// import Footer from "./footer";
 import Contact from "./contact";
+// import previewImage from "../images/preview.png";
+import Experience from "./experience";
+import Education from "./education";
 
-const inter = Inter({ subsets: ["latin"] });
+const Layout = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
-export default function Home() {
   return (
     <>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <h1>Hello Next World!</h1>
-          <p className={styles.description}>Get started by editing </p>
-          <h2>Learn</h2>
-          <ul></ul>
-        </div>
+      {/* <Helmet
+        title="👑CRod's Portfolio"
+        meta={[
+          {
+            property: `og:title`,
+            content: "👑CRodDev's Portfolio",
+          },
+          {
+            property: `og:description`,
+            content:
+              "I'm a Software Engineer with a passion for learning new things, creativity and innovation. I love to build things that make a difference. I hope you enjoy my work!",
+          },
+          {
+            property: `og:image`,
+            content: previewImage,
+          },
+        ]}
+      /> */}
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Josefin%20Sans:wght@400;500;700&display=swap"
+        rel="stylesheet"
+      />
+
+      {/* <Header /> */}
+      <main>
+        {/* <About /> */}
         <Experience />
         <Education />
         <Projects />
-
         <Skills />
         <Contact />
+        {/* <Connect /> */}
+        <div className="scroll-container z-50 opacity-30 transition-opacity duration-600 ease-in-out hover:opacity-100">
+          <div className="scroll-top bounce">
+            <a
+              aria-label="Scroll up"
+              href="#top"
+              onClick={scrollToTop}
+              className="text-white bg-blue-500 rounded-full p-3"
+            >
+              {/* <FontAwesomeIcon icon={faCircleArrowUp} /> */}
+            </a>
+          </div>
+        </div>
       </main>
+      {/* <Footer /> */}
     </>
   );
-}
+};
+
+export default Layout;
